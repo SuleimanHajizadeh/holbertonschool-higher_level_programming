e7a252c225678db635287fda506c5056a56dfa85
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-10-24 19:40:02 +0000

    sd

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
-    print("{}".format(chr(97 + i) if i % 2 == 0 else chr(65 + i)), end="")
+    j = 25 - i
+    print("{}".format(chr(122 - i) if j % 2 == 0 else chr(90 - i)), end="")
