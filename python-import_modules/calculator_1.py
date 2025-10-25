ea4212f0e31c39c604a09849df52f74f8bf821d4
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-10-25 16:42:48 +0000

    sd

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,51 +1,24 @@
 #!/usr/bin/python3
+"""Calculator functions: add, sub, mul, div."""
+
+
 def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+    """Return the addition of a and b."""
+    return a + b
 
 
 def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
+    """Return the subtraction of a and b."""
+    return a - b
 
 
 def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a "*" b)
+    """Return the multiplication of a and b."""
+    return a * b
 
 
 def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
+    """Return the integer division of a by b."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a // b
