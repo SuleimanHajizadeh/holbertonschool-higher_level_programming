6b228bb9d51ef1b8fc3fb2b20debf156b33c6913
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-10-25 17:24:28 +0000

    sd

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     if len(argv) != 4:
-        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     if argv[2] not in "+-*/":
-        print("{}".format("Unknown operator. Available operators: +, -, * and /"))
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
     a = int(argv[1])
