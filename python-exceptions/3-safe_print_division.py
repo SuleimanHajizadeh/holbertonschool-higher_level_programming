5fea0c7d0ae2c8d0a770958d14929f8d4a76c657
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-01 16:27:07 +0000

    sds

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print x elements of a list.
+# demonstrates how to use a try ... except for exception handling
+#
+# (C) 2024 Dejvid Papa, Tirana ,Albania
+# email dejvidpapa@gmail.com
+# -----------------------------------------------------------
 def safe_print_division(a, b):
     try:
-        result = format(a // b, '.1f')
-        print("Inside result:", end='')
-    except:
-        result = None
-        print("Inside result:", end='')
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
     finally:
-        print(" {}".format(result))
-    return result
+        print("Inside result: {}".format(div))
+    return div
