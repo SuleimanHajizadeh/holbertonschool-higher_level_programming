ce3bcaca5921e2724e97fa0954bab150db64519d
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-01 16:28:04 +0000

    sds

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print x elements of a list.
-# demonstrates how to use a try ... except for exception handling
-#
-# (C) 2024 Dejvid Papa, Tirana ,Albania
-# email dejvidpapa@gmail.com
-# -----------------------------------------------------------
 def safe_print_division(a, b):
     try:
         div = a / b
