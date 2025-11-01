dd16baaa0724f6a061a716d40d8341948cf4017e
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-01 18:25:20 +0000

    sds

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,26 @@
-#!/usr/bin/python3
-import sys
-
-def safe_print_integer_err(value):
-    """Print an integer. If not, print error to stderr."""
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError) as e:
-        print("Exception:", e, file=sys.stderr)
-        return False
+#!/usr/bin/python3 
+ # 100-safe_print_integer_err.py 
+ # Brennan D Baraban <375@holbertonschool.com> 
+  
+ import sys 
+  
+  
+ def safe_print_integer_err(value): 
+     """Prints an integer with "{:d}".format(). 
+  
+     If a ValueError message is caught, a corresponding 
+     message is printed to standard error. 
+  
+     Args: 
+         value (int): The integer to print. 
+  
+     Returns: 
+         If a TypeError or ValueError occurs - False. 
+         Otherwise - True. 
+     """ 
+     try: 
+         print("{:d}".format(value)) 
+         return (True) 
+     except (TypeError, ValueError): 
+         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr) 
+         return (False)
