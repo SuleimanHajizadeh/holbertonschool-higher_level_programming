9062873b2fcf1f15c22414b7b6a5e93a99fd13de
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-01 19:39:25 +0000

    sd

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Module for dividing all elements of a matrix by a number"""
+"""
+This is the "matrix_divided" module.
+"""
+
 
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by div
+    """Return a new matrix with divided and rounded values."""
 
-    matrix must be a list of lists of integers/floats
-    Each row must be of the same size
-    div must be a number different from 0
-    Returns a new matrix with all values divided and rounded to 2 decimals
-    """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(all(isinstance(x, (int, float)) for x in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if len(set(len(row) for row in matrix)) > 1:
+    if (not isinstance(matrix, list) or
+            not all(isinstance(r, list) for r in matrix) or
+            not all(isinstance(n, (int, float))for r in matrix for n in r)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
+
+    if len(matrix) == 0 or any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(x / div, 2) for x in row] for row in matrix]
+    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+    return (new_matrix)
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Modul for prints My name is <first name> <last name>"""
+"""
+This is the "say_my_name" module.
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """A function for prints My name is <first name> <last name>"""
+    """Print 'My name is <first_name> <last_name>'."""
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if first_name is None:
-        raise TypeError("say_my_name() missing 1 required"
-                        "positional argument: 'first_name")
     print("My name is {} {}".format(first_name, last_name))
