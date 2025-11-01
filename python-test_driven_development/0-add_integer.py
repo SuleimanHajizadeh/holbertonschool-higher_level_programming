cc9601708fb3ae14b0db59652927f556ecdaf74d
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-01 19:25:07 +0000

    sd

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+"""
+This is the "add_integer" module.
+"""
+
+
 def add_integer(a, b=98):
-    """Function return the result of addition of a and b
-    Args:
-        a(int) -> The first arg, must be passed
-        b(int) -> The second arg, if not passed, 98 is assumed to be the value
-    Returns an int
-    """
-    try:
-        if not (isinstance(a, int)) and not isinstance(a, float):
-            raise TypeError("a must be an integer")
-        elif not (isinstance(b, int)) and not isinstance(b, float):
-            raise TypeError("b must be an integer")
-        else:
-            if isinstance(a, float) or isinstance(b, float):
-                return (int(a) + int(b))
-            return a + b
-    except Exception as e:
-        return (e)
+    """Return the integer addition of a and b."""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    if (
+        a != a or b != b
+        or a in (float('inf'), float('-inf'))
+        or b in (float('inf'), float('-inf'))
+    ):
+        raise OverflowError("cannot convert float NaN or infinity to integer")
+
+    return (int(a) + int(b))
