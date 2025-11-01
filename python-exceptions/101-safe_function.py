59182887d34ad5a7088bf72734601357c4cacdff
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-01 18:27:27 +0000

    sds

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
+
 
 def safe_function(fct, *args):
-    """Executes a function safely, prints error to stderr if occurs."""
+    import sys
     try:
-        return fct(*args)
-    except Exception as e:
-        print("Exception:", e, file=sys.stderr)
-        return None
+        return (fct(*args))
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return (None)
