622b0b8dff275f633608b9530b0d95c83777e287
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-01 19:28:55 +0000

    sd

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""Modul for divides all elements of a matrix"""
-
+"""Module for dividing all elements of a matrix by a number"""
 
 def matrix_divided(matrix, div):
-    """This function divides all elements of a matrix"""
+    """Divides all elements of a matrix by div
 
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+    matrix must be a list of lists of integers/floats
+    Each row must be of the same size
+    div must be a number different from 0
+    Returns a new matrix with all values divided and rounded to 2 decimals
+    """
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(all(isinstance(x, (int, float)) for x in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if len(set(len(row) for row in matrix)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    if not all(len(matrix[0]) == len(matrix[i]) for i in range(len(matrix))):
-        raise TypeError("Each row of the matrix must have the same size")
-    new = []
-    a = "matrix must be a matrix (list of lists) of integers/floats"
-    for row in matrix:
-        new_row = []
-        for element in row:
-            if not isinstance(element, (int, float)):
-                raise TypeError(a)
-            element = round(float(element) / div, 2)
-            new_row.append(element)
-        new.append(new_row)
-    return new
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(x / div, 2) for x in row] for row in matrix]
