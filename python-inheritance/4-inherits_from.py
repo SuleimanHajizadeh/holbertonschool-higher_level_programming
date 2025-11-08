4e747bb4364cbaef8e3b3c0f45be5ae2b6c7915d
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-08 18:35:11 +0000

    sd

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,6 @@ It returns True only if the object is a subclass, not the same class.
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that inherited from a_class."""
+    """Return True if obj is an instance of a class that inherited from
+    a_class (not the same class)."""
     return issubclass(type(obj), a_class) and type(obj) is not a_class
