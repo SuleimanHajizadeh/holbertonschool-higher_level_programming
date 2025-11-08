b4d5ad774189be096cf0b8ccc2b8becebda3d6d8
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-08 16:15:59 +0000

    sd

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]"""
+    for r, c in board:
+        # eyni sütun
+        if col == c:
+            return False
+        # eyni diaqonal
+        if abs(row - r) == abs(col - c):
+            return False
+    return True
+
+def solve_nqueens(N, board=[], row=0):
+    """Backtracking solver"""
+    if row == N:
+        print(board)
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col):
+            solve_nqueens(N, board + [[row, col]], row + 1)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(N)
+
+if __name__ == "__main__":
+    main()
