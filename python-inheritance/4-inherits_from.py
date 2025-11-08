ab017f98430deb7a7fd78cd3f0c214155f0add4e
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-08 18:34:24 +0000

    sd

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module to check if an object is a subclass of a specified class."""
+"""Module to check if an object is a subclass of a specified class.
+It returns True only if the object is a subclass, not the same class.
+"""
 
 
 def inherits_from(obj, a_class):
