dd898fb1bcdd9e4b7186d08076967e4769c26aad
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-08 18:25:49 +0000

    sd

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,6 @@ class MyList(list):
     """MyList class that inherits from list."""
 
     def print_sorted(self):
-        """Prints the list in ascending order without modifying the original."""
+        """Prints the list in ascending order without modifying
+        the original."""
         print(sorted(self))
