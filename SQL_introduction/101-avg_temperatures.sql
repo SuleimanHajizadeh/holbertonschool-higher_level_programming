6607fa2c4989bf5ebc41fc8bae1ea80c89abb404
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-15 21:37:24 +0000

    sd

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- 101-avg_temperatures.sql
 USE hbtn_0c_0;
 
-SELECT city, AVG(temp) AS avg_temp
+SELECT city, ROUND(AVG(temp), 4) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
