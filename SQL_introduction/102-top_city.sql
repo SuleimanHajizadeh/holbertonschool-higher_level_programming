68969e0486f6f432a5578945d4ea4711e7cda875
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-15 21:43:20 +0000

    sd

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,9 +1,11 @@
--- 102-top_city.sql
+-- Select the database
 USE hbtn_0c_0;
-
-SELECT city, ROUND(AVG(temp), 4) AS avg_temp
-FROM temperatures
-WHERE MONTH(CONVERT(date, DATE)) IN (7, 8)
+-- Selects the 'city' and the average 'temperature' from 'temperatures' table for the months 'July' and 'August'
+-- Groups the result by 'city' and orders it by the average temperature in descending order
+-- Limits the result to the top 3 cities with the highest average temperature
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures -- Replace 'temperatures' with the actual name of your table
+WHERE month IN (7, 8) -- Adjust this condition based on the format of your 'month' column
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
