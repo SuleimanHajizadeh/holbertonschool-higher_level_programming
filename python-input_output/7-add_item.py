d4a02f39fbf04f2f5db3df380017833d04215e17
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-15 16:36:01 +0000

    sd

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-Bu skript komanda xəttindən verilən arqumentləri oxuyur,
-mövcud add_item.json faylından list yükləyir,
-yeni elementləri əlavə edir və yenilənmiş list-i JSON formatında
-fayla yazır.
-"""
-
+# 7-add_item.py
+# Suleyman Hajizada
+"""Add all arguments to a Python list, and then save them to a file."""
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-filename = "add_item.json"
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
-try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
-
-# Komanda xəttindən verilən arqumentləri list-ə əlavə edirik
-items.extend(sys.argv[1:])
-
-# Yenilənmiş list-i fayla yazırıq
-save_to_json_file(items, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
