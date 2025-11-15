abf7db673835311d0dbb1379d01e21fbdbfde507
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-15 16:46:20 +0000

    sd

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Bu modul Student class-ını ehtiva edir.
+Student class-ı tələbənin first_name, last_name və age atributlarını saxlayır.
+to_json metodu obyektin dictionary nümayəndəliyini qaytarır.
+attrs parametri ilə yalnız seçilmiş atributlar qaytarıla bilər.
+"""
+
+
+class Student:
+    """
+    Student class-ı tələbənin məlumatlarını saxlayır.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Student obyektini first_name, last_name və age ilə yaradır.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Obyektin atributlarını dictionary formatında qaytarır.
+        Əgər attrs listdirsə, yalnız bu listdəki atributlar daxil edilir.
+        Əks halda, bütün atributlar qaytarılır.
+        """
+        if isinstance(attrs, list):
+            filtered = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    filtered[key] = self.__dict__[key]
+            return filtered
+        return self.__dict__.copy()
