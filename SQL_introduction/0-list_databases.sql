4fee55ea093072fd2e2f608d4a8a8e20f2d948cf
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-15 20:39:56 +0000

    sd

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
 -- 0-list_databases.sql
--- Bu skript MySQL serverindəki bütün verilənlər bazalarını göstərir
-SHOW DATABASES;
+-- Bu skript "my_db" adlı verilənlər bazasının olub olmadığını yoxlayır və yoxdursa yaradır
+CREATE DATABASE IF NOT EXISTS my_db;
