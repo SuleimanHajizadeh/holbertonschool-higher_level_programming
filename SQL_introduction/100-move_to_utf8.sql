03b2eed1efed3389f5e4b51479c499fb629a2bb7
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-15 21:16:56 +0000

    sd

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -4,8 +4,11 @@
 -- Database charset və collate dəyişir
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
+-- Database seçilir
+USE hbtn_0c_0;
+
 -- Table charset və collate dəyişir
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Field name charset və collate dəyişir (əgər table convert etməsə belə)
+-- Field name charset və collate dəyişir
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
