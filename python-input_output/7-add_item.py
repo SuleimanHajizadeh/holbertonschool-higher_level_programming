64753032e43a49d4eb0b5789012191d4aa0cba3e
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-15 16:33:38 +0000

    sd

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Bu skript komanda xəttindən verilən arqumentləri oxuyur,
+mövcud add_item.json faylından list yükləyir,
+yeni elementləri əlavə edir və yenilənmiş list-i JSON formatında
+fayla yazır.
+"""
+
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+# Komanda xəttindən verilən arqumentləri list-ə əlavə edirik
+items.extend(sys.argv[1:])
+
+# Yenilənmiş list-i fayla yazırıq
+save_to_json_file(items, filename)
