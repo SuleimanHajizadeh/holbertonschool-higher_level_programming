279490e8980344e5ea2f6bfae5cda1ffa87f4c0b
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-15 16:47:58 +0000

    sd

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -4,6 +4,8 @@ Bu modul Student class-ını ehtiva edir.
 Student class-ı tələbənin first_name, last_name və age atributlarını saxlayır.
 to_json metodu obyektin dictionary nümayəndəliyini qaytarır.
 attrs parametri ilə yalnız seçilmiş atributlar qaytarıla bilər.
+reload_from_json metodu JSON dictionary ilə obyektin atributlarını yeniləyir.
+Diskə saxlama və yükləmə üçün JSON serializasiya ilə istifadə edilə bilər.
 """
 
 
@@ -33,3 +35,15 @@ class Student:
                     filtered[key] = self.__dict__[key]
             return filtered
         return self.__dict__.copy()
+
+    def reload_from_json(self, json_dict):
+        """
+        Obyektin atributlarını JSON dictionary ilə yeniləyir.
+        Yalnız obyektin mövcud atributları dəyişir.
+        json_dict: dictionary tipində, açarlar atribut adları,
+        dəyərlər isə atribut dəyərləri olmalıdır.
+        """
+        if isinstance(json_dict, dict):
+            for key, value in json_dict.items():
+                if key in self.__dict__:
+                    setattr(self, key, value)
