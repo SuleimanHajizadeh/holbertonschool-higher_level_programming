afa9bc5d72c48a879b131685b3fb342d8f6afb0f
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-11-23 13:54:17 +0000

    sd

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,23 +5,28 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Komanda sətrindən arqumentləri oxuyuruq
     user = sys.argv[1]
     passwd = sys.argv[2]
     db_name = sys.argv[3]
 
-    # MySQL serverinə qoşuluruq
-    conn = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db_name)
+    # Connect to MySQL server
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db_name
+    )
     cur = conn.cursor()
 
-    # states cədvəlindən bütün məlumatları id üzrə artan sıra ilə götürürük
+    # Execute query to get all states ordered by id
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
 
-    # Nəticələri çap edirik
+    # Print results
     for row in rows:
         print(row)
 
-    # Bağlantını bağlayırıq
+    # Close cursor and connection
     cur.close()
     conn.close()
